569c0ca1fc35332da89c48fc4373756fdb4c7342
icedwater <icedwater@gmail.com>
2020-02-23 00:48:06 +0800

    Added delays for --durations=N.

--- ch1/test_one.py
+++ ch1/test_one.py
@@ -1,2 +1,5 @@
+import time
+
 def test_passing():
+    time.sleep(0.16)
     assert (1, 2, 3) == (1, 2, 3)
--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,2 +1,5 @@
+import time
+
 def test_failing():
+    time.sleep(0.25)
     assert (1, 2, 3) == (3, 2, 1)
