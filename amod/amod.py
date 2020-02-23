998e762c0474fcf6c3e5faae9adcc7928e94e366
icedwater <icedwater@gmail.com>
2020-02-23 17:24:49 +0800

    Updated docstrings for amod and setup files.

--- amod/amod.py
+++ amod/amod.py
@@ -1,4 +1,8 @@
-# Following Appendix 4 on Packaging and Distributing
+# This module was developed following Appendix 4 on Packaging and
+# Distributing Python Projects in the PyTest book by Brian Okken.
+# It seems that while pip can install the module, uninstalling it
+# leaves it still importable because of __name__ being defined in
+# setup.py.
 
 def ans_all():
     return 42
--- amod/setup.py
+++ amod/setup.py
@@ -1,3 +1,6 @@
+# This is the setup file which allows pip to install and uninstall
+# the module "amod". Strangely, no clean uninstall is possible.
+
 from setuptools import setup
 
 setup(
